6974a4953f7be05c9584b5df9fdb02c7ab6d4caf
Thanh Nguyen <thanh.nguyen@berkeley.edu>
2019-04-21 18:55:37 -0700

    working on builting hallways

--- proj3/byow/Core/Room.java
+++ proj3/byow/Core/Room.java
@@ -31,7 +31,30 @@ public class Room {
     }
     */
 
-    static void connect(Room one, Room two) {}
+    static void connect(Room one, Room two) {
+        if (((one.x < two.x + two.w) && (two.x + two.w <= one.w + one.x))
+                || (one.x <= two.x && (two.x < one.x + one.w))) {
+            Hallway.builtVertical(one, two);
+        } else if () {
+
+        }
+        else if (one.x >= two.x + one.w) {
+            if (one.y + one.h <= two.y) {
+                Hallway.forwardL(two, one);
+            } else {
+                Hallway.backwardL(two, one);
+            }
+
+        } else if(one.x + one.w <= two.x) {
+             if (one.y + one.h <= two.y) {
+                 Hallway.bacdwardL(one, two);
+             } else {
+                 Hallway.forwardL(one, two);
+             }
+        }
+
+
+    }
     static boolean intersect(Room one, Room two) {
         return false;
     }
