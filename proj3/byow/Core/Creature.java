7e417064c4512404a8e6354f7b22582d07358380
Thanh Nguyen <thanh.nguyen@berkeley.edu>
2019-05-05 03:05:46 -0700

    add setX setY in creature

--- proj3/byow/Core/Creature.java
+++ proj3/byow/Core/Creature.java
@@ -40,6 +40,14 @@ abstract class Creature implements Serializable {
         return y;
     }
 
+    void setX(int newX) {
+        x = newX;
+    }
+
+    void setY(int newY) {
+        y = newY;
+    }
+
     int nextX() {
         switch (this.facing) {
             case RIGHT: return getX() + 1;
